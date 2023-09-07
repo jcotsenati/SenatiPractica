3960c91cd32d02755adfaf38a8b103a92a1d0144
jumanor <jumanor@gmail.com>
2023-09-07 18:14:00 -0500

    add Alumno

---
+++ WindowsFormsApp3/datos/alumno/DatosAlumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.datos
+{
+    internal class DatosAlumno
+    {
+        public EntidadAlumno ObtenerAlumno(int idAlumno)
+        {
+            return null;
+        }
+        public void InsertarAlumno(EntidadAlumno alumno) { 
+        
+
+        }
+        public void EditarAlumno(EntidadAlumno alumno)
+        {
+
+        }
+        public void EliminarAlumno(int idAlumno)
+        {
+
+        }
+    }
+}
